163e97552619483f6ee5244a9488ca8e1cfb7db4
LenWinkler <lenwinkler86@gmail.com>
2020-02-19 17:27:17 -0500

    started stock_prices fn

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,31 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  
+  current_i = 0
+  proceed = True
+
+  # using for loop, and starting i at prices[0], subtract price from each subsequent item in the list
+  while proceed:
+    # keep track of current_min_price_so_far, max_profit_so_far, and current index
+    current_min_price_so_far = prices[0]
+    max_profit_so_far = prices[1] - prices[0]
+
+    if current_i == len(prices) - 1 and prices[current_i] - current_min_price_so_far > max_profit_so_far:
+      max_profit_so_far = prices[current_i] - current_min_price_so_far
+    for price in prices:
+      j = current_i + 1
+      diff = prices[j] - prices[current_i]
+
+  # each time, the current result is compared to max_profit_so_far. if result > max, set max to result
+      if diff > max_profit_so_far:
+        max_profit_so_far = diff
+
+  # move j to the right
+      j += 1
+
+  # when j reaches end of list, move current_i to the right
+
 
 
 if __name__ == '__main__':
