5632d01cc7e9139731a074e7d1e9f8320a300d75
LenWinkler <lenwinkler86@gmail.com>
2020-02-20 00:21:20 -0500

    wrote pseudocode for recipe_batches and started coding

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,8 +3,26 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
 
+# create arr for recipe and arr for ingredients
+  recipe_keys = recipe.keys()
+  ingredients_keys = ingredients.keys()
+  print(recipe_keys)
+  print(ingredients_keys)
+
+# need to make sure ingredients contains all keys from recipe, else return 0
+
+  if len(ingredients_keys) < len(recipe_keys):
+    batches = 0
+  
+# loop over ingredients using if <ingredient from recipe> in ingredients: continue, else: return 0
+# will need a batch variable
+  batches = 0
+# divide each key in ingredients by its relative in recipe
+# if any result == 0, return 0
+# lowest remainder from the group is the # of batches
+
+  return batches
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
